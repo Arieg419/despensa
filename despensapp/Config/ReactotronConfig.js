15ace8379cbbc91d73564e2dedfbdb762caa3882
harrisrobin <harrisrobinkalash@gmail.com>
2017-10-07 17:26:24 -0400

    re-add reactotron

--- despensapp/Config/ReactotronConfig.js
+++ despensapp/Config/ReactotronConfig.js
@@ -1,22 +1,21 @@
-import Config from "./DebugConfig";
-import Reactotron from "reactotron-react-native";
-Reactotron.log("hello rendering world");
-import { reactotronRedux as reduxPlugin } from "reactotron-redux";
+import Config from "./DebugConfig"
+import Reactotron from "reactotron-react-native"
+Reactotron.log("hello rendering world")
+import { reactotronRedux as reduxPlugin } from "reactotron-redux"
 
-// if (Config.useReactotron) {
-//   console.log("hello reactotron");
-Reactotron.configure({ name: "Despensa App" })
-  .useReactNative()
-  .use(reduxPlugin())
-  .connect();
-//
-//   // Let's clear Reactotron on every time we load the app
-//   if (__DEV__) {
-Reactotron.connect();
-Reactotron.clear();
-//   }
-//
-//   // Totally hacky, but this allows you to not both importing reactotron-react-native
-//   // on every file.  This is just DEV mode, so no big deal.
-console.tron = Reactotron;
-// }
+if (Config.useReactotron) {
+  Reactotron.configure({ name: "Despensa App" })
+    .useReactNative()
+    .use(reduxPlugin())
+    .connect()
+  //
+  //   // Let's clear Reactotron on every time we load the app
+  if (__DEV__) {
+    Reactotron.connect()
+    Reactotron.clear()
+  }
+  //
+  //   // Totally hacky, but this allows you to not both importing reactotron-react-native
+  //   // on every file.  This is just DEV mode, so no big deal.
+  console.tron = Reactotron
+}
