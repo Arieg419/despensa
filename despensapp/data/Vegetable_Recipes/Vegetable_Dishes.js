32a425b8d9b7ee026fd410bb17f51d22863bf8a8
Omer Goldberg <arieg419@gmail.com>
2017-11-02 00:09:43 +0200

    dish type selector with state is working, should be changed so that parent passes state to child

---
+++ despensapp/data/Vegetable_Recipes/Vegetable_Dishes.js
@@ -0,0 +1,22 @@
+const Vegetable_Dishes = [
+  "Artichokes",
+  "Asparagus",
+  "Cabbage",
+  "Carrots",
+  "Cauliflower",
+  "Celery",
+  "Corn",
+  "Eggplant",
+  "Endive",
+  "Fennel",
+  "Greens",
+  "Mushrooms",
+  "Onions",
+  "Peppers",
+  "Potatoes",
+  "Spinach",
+  "Tomatoes",
+  "Zucchini"
+];
+
+export default Vegetable_Dishes;
